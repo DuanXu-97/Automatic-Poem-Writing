07c323bbb5f36b268f92004522ca870ae8b58e8f
DuanXu-97 <956378834@qq.com>
2020-05-07 00:45:33 +0800

    update

--- models/network.py
+++ models/network.py
@@ -28,34 +28,26 @@ class AttLSTM(BasicModule):
         self.fc = nn.Linear(self.config.hidden_dim, self.config.vocab_size)
 
     def attention(self, lstm_output, final_state):
-        print(lstm_output.size())
-        print(final_state.size())
-
         # [seq_len, batch, hidden_size]->[batch, seq_len, hidden_size]
         lstm_output = lstm_output.permute(1, 0, 2)
         batch_size, seq_len, hidden_size = lstm_output.size()
-        print(lstm_output.size())
 
         # [num_layers, batch, hidden_size]->[batch, hidden_size]
         merged_state = t.mean(final_state, 0)
-        print(merged_state.size())
 
         # [batch, hidden_size]->[batch, hidden_size, 1]
         merged_state = merged_state.squeeze(0).unsqueeze(2)
-        print(merged_state.size())
 
         ogema = nn.Parameter(t.zeros(batch_size, hidden_size, hidden_size), requires_grad=True).cuda()
         # [batch, hidden_size, hidden_size], [batch, hidden_size, 1] -> [batch, hidden_size, 1]
         weights = t.bmm(ogema, merged_state)
         weights = F.softmax(weights.squeeze(2)).unsqueeze(2)
-        print('att_weights', weights.size())
         # [batch, seq_len, hidden_size] * [batch, seq_len, hidden_size] -> [batch, seq_len, hidden_size]
         out = lstm_output.mul(weights.transpose(1, 2).repeat(1, seq_len, 1))
         return out
 
     def forward(self, x, hidden=None):
         seq_len, batch_size = x.size()
-        print(x.size())
         if hidden is None:
             h_0 = x.data.new(2, batch_size, self.config.hidden_dim).fill_(0).float()
             c_0 = x.data.new(2, batch_size, self.config.hidden_dim).fill_(0).float()
@@ -66,7 +58,6 @@ class AttLSTM(BasicModule):
         x = self.embeddings(x)
         lstm_output, lstm_hidden = self.lstm(x, (h_0, c_0))
         x = self.attention(lstm_output, lstm_hidden[0])
-        print(x.size())
 
         output = self.fc(x.reshape(seq_len*batch_size, -1))
         return output, lstm_hidden
