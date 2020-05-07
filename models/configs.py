62b2350f2c7cd33f9b1fd5a5353bc3fdf35ccf57
DuanXu-97 <956378834@qq.com>
2020-05-07 20:34:06 +0800

    update

--- models/configs.py
+++ models/configs.py
@@ -85,8 +85,7 @@ class BiGRUConfig(object):
 
     num_layers = 2
     embedding_dim = 128
-    hidden_dim_1 = 128
-    hidden_dim_2 = 256
+    hidden_dim = 256
     dropout_rate = 0.05
     max_len = 125
 
@@ -96,5 +95,28 @@ class BiGRUConfig(object):
     seed = 10
 
 
+class ShortcutGRUConfig(object):
+    load_model_path = None
+    use_gpu = True
+    num_workers = 2
+    print_freq = 10
+
+    data_path = "../data/tang.npz"
+
+    vocab_size = 0
+    ix2word = None
+    word2ix = None
+
+    num_layers = 2
+    embedding_dim = 128
+    hidden_dim_1 = 128
+    hidden_dim_2 = 256
+    dropout_rate = 0.05
+    max_len = 125
+
+    batch_size = 128
+    epoch = 20
+    lr = 0.001
+    seed = 10
 
 
