4007d149198753abcd8deac44a7ee9e9671d7e03
DuanXu-97 <956378834@qq.com>
2020-05-11 19:27:10 +0800

    update

--- models/test.py
+++ models/test.py
@@ -114,7 +114,7 @@ if __name__ == '__main__':
     parser.add_argument('--use_gpu', action='store_true', help="whether use gpu")
     parser.add_argument('--load_model_path', type=str, required=True, help="Path of trained model")
     parser.add_argument('--acrostic', action='store_true', help="whether generate acrostic poem")
-    parser.add_argument('--given_words', type=str, default='雁栖湖畔', help="given words to generate poem")
+    parser.add_argument('--given_words', type=str, default='深度学习', help="given words to generate poem")
     parser.add_argument('--prefix_words', type=str, default='孤舟蓑笠翁，独钓寒江雪。', help="given words to generate poem")
 
     args = parser.parse_args()
